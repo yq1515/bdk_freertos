fcb53db17f6f08cb64bf8283d05ddcb54c266689
tiancj <cj.tian.seu@gmail.com>
2023-11-01 09:13:51 +0800

    remove unused files

--- beken378/alios/entry/entry.mk
+++
@@ -1,9 +0,0 @@
-NAME := entry
-
-$(NAME)_TYPE := kernel
-
-$(NAME)_CFLAGS += -marm
-                   
-$(NAME)_SOURCES	 := boot_handlers.S \
-                    boot_vectors.S \
-                    ll.S 
