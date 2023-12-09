92d0bd2578602459cefafef4ee2eebefcb0ec140
tiancj <cj.tian.seu@gmail.com>
2023-12-09 20:20:55 +0800

    disable functions

--- driver/entry/arch_main.c
+++ driver/entry/arch_main.c
@@ -22,13 +22,13 @@ uint32_t  extended_app_stack_size = 2048;
 extern void user_main_entry(void);
 
 void extended_app_launch_over(void)
-{  
+{
 	OSStatus ret;
 	ret = rtos_set_semaphore(&extended_app_sema);
-	
+
 	(void)ret;
 }
-    
+
 void extended_app_waiting_for_launch(void)
 {
 	OSStatus ret;
@@ -42,7 +42,7 @@ void extended_app_waiting_for_launch(void)
 static void extended_app_task_handler(void *arg)
 {
     /* step 0: function layer initialization*/
-    func_init_extended();  
+    func_init_extended();
 
     /* step 1: startup application layer*/
     if(get_ate_mode_state())
@@ -53,32 +53,32 @@ static void extended_app_task_handler(void *arg)
     {
 	    app_start();
     }
-         
+
 	extended_app_launch_over();
-	
+
     rtos_delete_thread( NULL );
 }
 
 void extended_app_init(void)
 {
 	OSStatus ret;
-	
-    ret = rtos_init_semaphore(&extended_app_sema, 1);	
+
+    ret = rtos_init_semaphore(&extended_app_sema, 1);
 	ASSERT(kNoErr == ret);
 }
 
 void extended_app_uninit(void)
 {
 	OSStatus ret;
-	
-    ret = rtos_deinit_semaphore(&extended_app_sema);	
+
+    ret = rtos_deinit_semaphore(&extended_app_sema);
 	ASSERT(kNoErr == ret);
 }
 
 void extended_app_launch(void)
 {
 	OSStatus ret;
-	
+
 	ret = rtos_create_thread(NULL,
 					   THD_EXTENDED_APP_PRIORITY,
 					   "extended_app",
@@ -89,28 +89,32 @@ void extended_app_launch(void)
 }
 
 void entry_main(void)
-{  
+{
     ate_app_init();
-	
-    bk_misc_init_start_type();
+
+    // bk_misc_init_start_type();
     /* step 1: driver layer initialization*/
     driver_init();
-	bk_misc_check_start_type();
+	//bk_misc_check_start_type();
 	func_init_basic();
 
-	extended_app_init();
+	//extended_app_init();
     /* step 2: user entry
        attention: if the user wants to increase boot time and light up the board, you may invoke the
-       			  routine:user_main_entry before invoking extended_app_launch; and then user MUST wait 
+       			  routine:user_main_entry before invoking extended_app_launch; and then user MUST wait
        			  for the init of most other devices, such as:wlan and so on;
-       			  
-       			  if you do not care the boot time, the function: extended_app_launch shall be invoked 
+
+       			  if you do not care the boot time, the function: extended_app_launch shall be invoked
        			  BEFORE user_main_entry;*/
-	user_main_entry();	
-	
+	//user_main_entry();
+
     /* step 3: init of the most of devices*/
-	extended_app_launch();
-	
+	//extended_app_launch();
+
+	while (1) {
+		os_printf("wait for xxx\n");
+		delay(10000);
+	}
 #if (CFG_OS_FREERTOS)
     vTaskStartScheduler();
 #endif
